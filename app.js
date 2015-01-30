6c438fd7980a9f5a3a545f4d075402708f45f7b2
drquinn <blong123@gmail.com>
2015-01-30 14:44:09 -0700

    added post to db

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 
 var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/emails');
+var db = require('monk')('localhost:27017/emails');
 var app = express();
 
 // bodyParser() gets the data from a POST.
@@ -14,17 +13,40 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use(function(req,res,next) {
-//    req.db = db;
-//    next();
-//});
+app.use(function(req,res,next) {
+    req.db = db;
+    next();
+});
 
 // Get an instance of the Express Router.
 var router = express.Router();
 
 // Get the email submission form.
-router.get('/getMeEmails', function (req, res) {
-    res.sendFile(path.join(__dirname, 'public', 'super.html'));
+router.get('/collect', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'collection.html'));
+});
+
+// Get the thank you page.
+router.get('/thankyou', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'thankyou.html'));
+});
+
+router.post('/addMail', function(req, res) {
+    var username = req.body.name;
+    var useremail = req.body.email;
+   
+    db.get('usercollection').insert({
+      "name" : username,
+      "email": useremail
+    }, function (err, doc) {
+      if (err) {
+         res.send("Error adding user to db.");
+      }
+      else {
+        res.location('thankyou');
+        res.redirect('thankyou');
+      }
+    }); 
 });
 
 // Tell express to use the route you just set up.
