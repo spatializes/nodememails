f341809e6dcccb29c494e196889f57bccce2b8b9
drquinn <blong123@gmail.com>
2015-01-30 18:17:08 -0700

    test values

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {};
 
-config.apiToken = "ignoreme";
+config.apiToken = "POSTMARK_API_TEST";
 config.toEmail = "test@test.test";
 config.fromEmail = "test@test.test";
 config.port = "3000";
